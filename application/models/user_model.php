a524b6aa4ccb2b337c65acb398056eb03df7552a
kikojumkd <junuzovski.kristijan@students.finki.ukim.mk>
2018-02-07 21:12:58 +0100

    finished login app

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -2,7 +2,7 @@
 
 class User_model extends CI_Model {
 
-    public function get_users($user_id, $username) {
+//    public function get_users($user_id, $username) {
 //        $config['hostname'] = "127.0.0.1";
 //        $config['username'] = "root";
 //        $config['password'] = "root";
@@ -14,30 +14,41 @@ class User_model extends CI_Model {
 //        return $query->num_rows(); //this will give me the number of rows in the table users
 //        return $query->num_fields(); //this will give me the number of columns in the table users
 //        return $query->result();
-        $this->db->where([
-            'idusers' => $user_id,
-            'username' => $username
-        ]);
+//        $this->db->where([
+//            'idusers' => $user_id,
+//            'username' => $username
+//        ]);
 //        $this->db->where('idusers', $user_id);
-        $query = $this->db->get('users');
-        return $query->result();
-    }
-    public function create_users($data) {
-        $this->db->insert('users', $data);
-    }
-    public function update_users($data, $id) {
-        $this->db->where([
-            'idusers' => $id
-        ]);
-        $this->db->update('users', $data);
-    }
-    public function delete_users($id) {
-        $this->db->where([
-            'idusers' => $id
-        ]);
-        $this->db->delete('users');
-    }
+//        $query = $this->db->get('users');
+//        return $query->result();
+    public function login_user($username, $password) {
+        $this->db->where('username', $username);
+        $this->db->where('password', $password);
+        $result = $this->db->get('users');
 
+        if($result->num_rows() == 1){
+            return $result->row(0)->idusers;
+        } else {
+            return false;
+        }
+
+    }
 }
+//    public function create_users($data) {
+//        $this->db->insert('users', $data);
+//    }
+//    public function update_users($data, $id) {
+//        $this->db->where([
+//            'idusers' => $id
+//        ]);
+//        $this->db->update('users', $data);
+//    }
+//    public function delete_users($id) {
+//        $this->db->where([
+//            'idusers' => $id
+//        ]);
+//        $this->db->delete('users');
+//    }
+
 
 ?>
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,6 +1,10 @@
+<?php if(!$this->session->userdata('logged_in')): ?>
 <h2>Login Form</h2>
 
 <?php $attributes = array('id' => 'login_form', 'class' => 'form-horizontal');?>
+<?php if($this->session->flashdata('errors')):?>
+<?php echo $this->session->flashdata('errors');?>
+<?php endif; ?>
 <?php echo form_open('users/login', $attributes);?>
 <div class="form-group">
     <?php echo form_label('Username');?>
@@ -25,6 +29,17 @@
     <?php echo form_password($data);?>
 </div>
 <div class="form-group">
+    <?php echo form_label('Confirm');?>
+    <?php
+    $data = array(
+        'class' => 'form-control',
+        'name' => 'confirm_password',
+        'placeholder' => 'Confirm Password'
+    );
+    ?>
+    <?php echo form_password($data);?>
+</div>
+<div class="form-group">
     <?php
     $data = array(
         'class' => 'btn btn-primary',
@@ -35,3 +50,23 @@
     <?php echo form_submit($data);?>
 </div>
 <?php echo form_close();?>
+
+    <?php else:  ?>
+    <h2>Logout</h2>
+    <?php echo form_open('users/logout'); ?>
+    <p>
+    <?php if($this->session->userdata('username')):?>
+    <?php echo "You are logged in as " . $this->session->userdata('username'); ?>
+    <?php endif;?>
+    </p>
+    <?php
+        $data = array(
+                'class' => 'btn btn-primary',
+                'name' => 'submit',
+                'value' => 'Logout'
+        );
+    ?>
+    <?php echo form_submit($data); ?>
+
+    <?php echo form_close(); ?>
+<?php endif;?>
